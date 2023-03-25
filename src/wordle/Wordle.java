4d9761c36531e10c7f1c4debdb7c8521941e04f8
Gyalbu Sherpa <gyalbusherpa555@gmail.com>
2023-03-25 13:50:38 +0545

    almost there

--- src/wordle/Wordle.java
+++ src/wordle/Wordle.java
@@ -1,21 +1,26 @@
-
-
 package wordle;
 
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 class Wordle {
-    public String[] words() {
-        String[] word = {"ABACK", "ABOVE", "ADMIT", "ADOPT", "ADORE",
-                "BIRTH", "BELLY", "BENCH", "CHOKE", "CHORD", "CHUNK", "ENJOY", "EPOCH", "FAULT", "FAVOR", "FEAST",
-                "GAMER", "GREAT", "GREET", "HUMAN", "HUMOR", "HORSE", "JUDGE", "KARMA", "IONIC", "IRONY", "ITCHY",
-                "LOFTY", "LOOPY", "LOSER"};
-        return word;
+    public String words() {
+        String[] word = {"ABOVE", "ADMIT", "ADOPT", "ADORE",
+                "BIRTH", "BENCH", "CHOKE", "CHORD", "CHUNK", "DRIVE", "DUTCH", "DRAIN", "ENJOY", "EPOCH", "FAULT",
+                "FAVOR", "FEAST", "GAMER", "GREAT", "HUMAN", "HUMOR", "HORSE", "JUDGE", "JOKER", "IRONY", "ITCHY",
+                "LOFTY", "LOSER", "MONTH", "MODEL", "OPERA", "PAUSE", "PURGE", "ROBIN", "ROUND",
+                "ROYAL", "SCOLD", "SHAKE", "SHAME", "SKIRT", "SMITE", "SNEAK", "SOLAR", "SOUND", "STORY",
+                "TOUGH", "TWICE", "UNIFY", "VIRAL", "VITAL", "WACKY", "WASTE", "WORSE", "YIELD", "YOUTH"};
+        int index = ThreadLocalRandom.current().nextInt(word.length);
+        return word[index];
     }
 
-    public int play(char[] character) {
+    public int play(char[] character, String solution) {
 
-        char[] answer = {'t', 't', 'a', 'l', 'k'};
+        char[] answer = new char[5];
+        for (int i = 0; i < solution.length(); i++) {
+            answer[i] = solution.charAt(i);
+        }
 
         for (int i = 0; i < 5; i++) {
             System.out.print("+---+ ");
@@ -42,6 +47,7 @@ class Wordle {
                 System.out.print("| " + Color.RED_BOLD + character[i] + Color.RESET + " | ");
             }
         }
+
         System.out.println();
         for (int i = 0; i < 5; i++) {
             System.out.print("+---+ ");
@@ -50,29 +56,49 @@ class Wordle {
         return count;
     }
 
+    public boolean playAgain(Scanner sc) {
+        System.out.println("Would you like to play again? (y/n)");
+        String playAgainOption = sc.nextLine().toLowerCase();
+        if (playAgainOption.equals("n")) {
+            System.out.println("Thanks for playing");
+            return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Wordle w = new Wordle();
+        boolean playAgain = true;
+        while (playAgain) {
+            String solution = w.words().toLowerCase();
+            System.out.println("Welcome, Enter a word ");
+            int count;
+            for (int j = 0; j < 6; j++) {
+                char[] userInput = new char[5];
 
-        for (int j = 0; j < 6; j++) {
-            char[] userInput = new char[5];
-            Scanner sc = new Scanner(System.in);
-
-            boolean validInput = false;
-            while (!validInput) {
-                String word = sc.nextLine().toLowerCase();
-                if (word.length() == 5) {
-                    for (int i = 0; i < word.length(); i++) {
-                        userInput[i] = word.charAt(i);
+                boolean validInput = false;
+                while (!validInput) {
+                    String word = sc.nextLine().toLowerCase();
+                    if (word.length() == 5) {
+                        for (int i = 0; i < word.length(); i++) {
+                            userInput[i] = word.charAt(i);
+                        }
+                        validInput = true;
+                    } else {
+                        System.out.println("Please enter 5 lettered word");
                     }
-                    validInput = true;
-                } else {
-                    System.out.println("Please enter 5 lettered word");
                 }
-            }
-            int count = w.play(userInput);
-            if (count == 5) {
-                System.out.println("You won!");
-                break;
+
+                count = w.play(userInput, solution);
+                if (count == 5) {
+                    System.out.println("You won!");
+                    playAgain = w.playAgain(sc);
+                    break;
+                } else if (j == 5) {
+                    System.out.println("You lost!");
+                    playAgain = w.playAgain(sc);
+                }
             }
         }
     }
