5a3e53e30b94a2e69fa28b16a37d48c20d470c87
Crack-way <rupeshsunuwar2019@gmail.com>
2023-03-26 09:40:04 +0545

    Changes in wordle class

--- src/wordle/Wordle.java
+++ src/wordle/Wordle.java
@@ -1,9 +1,125 @@
 package wordle;
-
+import java.util.Arrays;
 import java.util.Scanner;
 
-class Wordle {
-    public static void main(String[] args) {
+public class Wordle {
+
+    public static String arr="rupes";
+
+    //converting string to character
+    public static char[]  arr1=arr.toCharArray();
+
+    public static char[]  arr2=new char[arr1.length];
+
+    public static String[] arr3=new String[arr1.length];
+
+    //Ansi for using the color in java
+    public static final String	BLACK				= "\u001B[30m";
+    public static final String	RED					= "\u001B[31m";
+    public static final String	GREEN				= "\u001B[32m";
+    public static final String	YELLOW				= "\u001B[33m";
+
+
+    public static int count=0;
+
+
+    public static void main(String[] args){
+
+
+        //for showing the pattern
+        do{
+            pattern(count);
+
+
+            Scanner sc=new Scanner(System.in);
+
+            System.out.println("Input your string");
+
+            String input=sc.nextLine();
+
+            checkWord(input);
+
+            outFormatter(arr3);
+
+            if(Arrays.equals(arr1,arr2)){
+
+                break;
+            }
+
+        }
+        while(count<6);
+    }
+
+    public static void pattern(int n){
+        int i=0;
+        System.out.println("");
+        if(n<=5) {
+            for ( i = 0; i < 5; i++) {
+                System.out.print("+---+ ");
+            }
+            System.out.println("");
+            for( i=0;i<5;i++){
+
+                System.out.print("|" + "  " +  " |");
+            }
+            System.out.println("");
+            for(  i=0;i<5;i++){
+                System.out.print("+---+ ");
+            }
+            pattern(n + 1);
+        }
+
+    }
+
+    public static void outFormatter(String[] str){
+
+
+        for(int i=0; i<5; i++){
+            System.out.print("+---+ ");
+        }
+        System.out.println();
+        for(int i=0; i<5; i++){
+            System.out.print("| "+str[i]+" | ");
+        }
+        System.out.println();
+        for(int i=0; i<5; i++) {
+            System.out.print("+---+ ");
+        }
+
+    }
+    public static void checkWord(String input){
+
+        count +=1;
+
+
+        arr2=input.toCharArray();
+
+
+        for(int j=0;j<arr2.length;j++){
+
+            char a=arr2[j];
+            String n=BLACK + a;
+
+            for(int k=0;k<arr2.length;k++){
+
+                if(a==arr1[k] && j==k){
+
+                    n= GREEN + a;
+                    arr3[j]=n;
+                    break;
+
+
+                }
+                else if(a==arr1[k] && j!=k){
+                    n= YELLOW + a ;
+                    arr3[j]=n;
+
+                    break;
+                }
+                arr3[j]=n;
+            }
+
+        }
 
     }
 }
